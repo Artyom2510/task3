5b434fa9c69b0e717f2d515104f78abb21a9b55a
Artyom <taranenko@grechka.digital>
2019-04-05 18:04:54 +0300

    связать по айди

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
-$(function() {
-
+$(document).ready(function(){
+	$('.datepicker').datepicker({
+		dateFormat : "yy-mm-dd",
+		monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+		dayNamesMin : ['Вс', 'Пн','Вт','Ср','Чт','Пт','Сб'],
+		dayNames: ['воскресенье', 'понедельник','вторник','среда','четверг','пятница','суббота'],
+		firstDay: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		minDate: 0
+	});
 });
