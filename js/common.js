c590833f222e2439f76f141af2764cbd69f45ace
Max Shevkoplias <shevkoplias.mk@yandex.ru>
2019-04-08 18:58:59 +0300

    продолжаю делать таймлайн

--- js/common.js
+++ js/common.js
@@ -35,13 +35,15 @@ $(function() {
 	var Timeline = window.Timeline || {};
 
 	Timeline = (function() {
-		function Timeline(element, settings) {
+		function Timeline(selector, settings) {
 			var _ = this;
 			
-			_.$timeline = $(element);
+			_.$timeline = $(selector);
+			_.$popup = $('.meeting-rooms-form-popup');
 
 			_.store = createStore(_.reducer, {
-				data: settings.data
+				startState: settings.data,
+				nextState: []
 			});
 
 			_.unsubscribe = _.store.subscribe(function () {
@@ -81,7 +83,7 @@ $(function() {
 		var $halves = $('<div class="timeline__halves"></div>');
 
 		var holdTimeList = [];
-		state.data.forEach(function(el) {
+		state.startState.forEach(function(el) {
 			for( var i = +el.from ; i < +el.to ; i++ ) {
 				holdTimeList.push(i);
 			}
@@ -112,15 +114,228 @@ $(function() {
 	}
 
 	Timeline.prototype.booking = function(time) {
-		console.log('Бронирую ' + time);
+		var _ = this;
+
+		// _.$popup.switchPopup('open');
+		_.renderNewSequence({
+			from: time,
+			to: time + 1
+		});
+	}
+
+	Timeline.prototype.renderNewSequence = function(interval) {
+		var _ = this;
+
+		_.renderSequence({
+			interval: interval,
+			update: function(state) {
+				_.updateNextState(state.nextState);
+			}
+		});
+		
+		_.updateNextState(interval);
+	}
+
+	Timeline.prototype.renderSequenceFromState = function() {
+		var _ = this;
+		var state = _.store.getState();
+
+		_.$timeline.children('.timeline__sequence').remove();
+
+		console.log('render all', state.nextState);
+		state.nextState.forEach(function(interval) {
+			var $sequence = _.renderSequence({
+				interval: interval,
+				update: function(state) {
+					_.updateNextState(state.nextState);
+				}
+			});
+			
+			_.$timeline.append($sequence);
+		});
+	}
+
+	Timeline.prototype.updateNextState = function(interval) {
+		var _ = this;
+
+		console.log('update', interval);
+		_.store.dispatch({
+			type: 'UPDATE_NEXT_STATE',
+			payload: _.validateTime(interval)
+		});
+		_.renderSequenceFromState();
+	}
+
+	Timeline.prototype.renderSequence = function(props) {
+		var interval = props.interval;
+		var update = props.update;
+
+		var $sequence = $('<div class="timeline__sequence"></div>');
+		var $sequenceStart = $('<span class="timeline__sequence-handle-start"></span>');
+		var $sequenceEnd = $('<span class="timeline__sequence-handle-end"></span>');
+
+		$sequence.append([
+			$sequenceStart,
+			$sequenceEnd
+		]);
+
+		var time = interval.from;
+		var count = interval.to - interval.from;
+
+		var sequenceWidthOne = 60; // Размер одного деления
+		var sequenceWidth = sequenceWidthOne * count; // Длина временного отрезка в пикселях
+		var sequencePositionLeft = sequenceWidthOne * time; // Позиция временного отрезка в пикселях
+		var sequenceWidthStart = sequenceWidth; // Начальная длина временного отрезка в пикселях (для изменения)
+		var sequencePositionLeftStart = sequencePositionLeft; // Начальная позиция временного отрезка в пикселях (для изменения)
+		var mouseDownStart = 0; // Координата начала движения мыши (для изменения)
+		var sequenceIsDown = false; // Передвигаю временной отрезок
+		var sequenceStartIsDown = false; // Изменяю длину временного отрезка (от начала)
+		var sequenceEndIsDown = false; // Изменяю длину временного отрезка (от конца)
+		var returnTime = time; // Возвращаемое начало временного отрезка
+		var returnCount = count; // Возвращаемое количество часов временного отрезка
+
+		var prevState = {
+			from: interval.from,
+			to: interval.to
+		};
+
+		$sequence[0].style.width = sequenceWidth + 'px';
+		$sequence[0].style.left = sequencePositionLeft + 'px';
+
+		$sequence.on('mousedown', function(e) {
+			var eClassList = e.target.classList;
+
+			if( eClassList.contains('timeline__sequence-handle-start') ) { // Начали тянуть левую часть
+				sequenceStartIsDown = true;
+				mouseDownStart = e.clientX;
+				sequenceWidthStart = parseInt($sequence[0].style.width);
+				sequencePositionLeftStart = parseInt($sequence[0].style.left);
+			} else if( eClassList.contains('timeline__sequence-handle-end') ) { // Начали тянуть правую часть
+				sequenceEndIsDown = true;
+				mouseDownStart = e.clientX;
+				sequenceWidthStart = parseInt($sequence[0].style.width);
+			} else { // Начали тянуть весь блок
+				sequenceIsDown = true;
+				mouseDownStart = e.clientX;
+				sequencePositionLeftStart = parseInt($sequence[0].style.left);
+			}
+			prevState = {
+				from: sequencePositionLeftStart / sequenceWidthOne,
+				to: (sequencePositionLeftStart + sequenceWidthStart) / sequenceWidthOne
+			};
+		});
+
+		$(document).on('mousemove', function(e) {
+			if( sequenceIsDown ) { // Зажата вся полоска
+				if( mouseDownStart >= e.clientX ) { // Тянем в лево
+					sequencePositionLeft = sequencePositionLeftStart - (mouseDownStart - e.clientX);
+				} else { // Тянем в право
+					sequencePositionLeft = sequencePositionLeftStart + (e.clientX - mouseDownStart);
+				}
+				$sequence[0].style.left = sequencePositionLeft + 'px';
+			}
+
+			if( sequenceStartIsDown ) { // Зажата левая тягалка
+				if( mouseDownStart >= e.clientX ) { // Тянем в лево
+					sequenceWidth = sequenceWidthStart + (mouseDownStart - e.clientX);
+					sequencePositionLeft = sequencePositionLeftStart - (mouseDownStart - e.clientX);
+				} else { // Тянем в право
+					sequenceWidth = sequenceWidthStart - (e.clientX - mouseDownStart);
+					sequencePositionLeft = sequencePositionLeftStart + (e.clientX - mouseDownStart);
+				}
+				$sequence[0].style.width = sequenceWidth + 'px';
+				$sequence[0].style.left = sequencePositionLeft + 'px';
+			}
+
+			if( sequenceEndIsDown ) { // Зажата правая тягалка
+				sequenceWidth = sequenceWidthStart + (e.clientX - mouseDownStart);
+				$sequence[0].style.width = sequenceWidth + 'px';
+			}
+		});
+
+		$(document).on('mouseup', function() {
+			if( sequenceIsDown || sequenceStartIsDown ) {
+				sequencePositionLeft = Math.round(sequencePositionLeft / sequenceWidthOne) * sequenceWidthOne;
+				$sequence[0].style.left = sequencePositionLeft + 'px';
+			}
+			if( sequenceEndIsDown || sequenceStartIsDown ) {
+				sequenceWidth = Math.round(sequenceWidth / sequenceWidthOne) * sequenceWidthOne;
+				$sequence[0].style.width = sequenceWidth + 'px';
+			}
+			if( sequenceIsDown || sequenceStartIsDown || sequenceEndIsDown ) {
+				returnTime = sequencePositionLeft / sequenceWidthOne;
+				returnCount = sequenceWidth / sequenceWidthOne;
+				update({
+					nextState: {
+						from: returnTime,
+						to: returnTime + returnCount
+					},
+					prevState: prevState
+				});
+	
+				sequenceIsDown = false;
+				sequenceEndIsDown = false;
+				sequenceStartIsDown = false;
+				mouseDownStart = 0;
+			}
+		});
+
+		return $sequence[0];
+	}
+
+	Timeline.prototype.validateTime = function(newInterval) {
+		var _ = this;
+		var state = _.store.getState();
+
+		var timeline = [];
+		var resultTimeline = [];
+		for( var i = 0 ; i < 24 ; i++ ) {
+			timeline[i] = 0;
+		}
+
+		state.startState.forEach(function(interval) {
+			for( var i = +interval.from ; i < +interval.to ; i++ ) {
+				timeline[i] = 1;
+			}
+		});
+
+		state.nextState.forEach(function(interval) {
+			for( var i = interval.from ; i < interval.to ; i++ ) {
+				timeline[i] = 2;
+			}
+		});
+
+		for( var i = newInterval.from ; i < newInterval.to ; i++ ) {
+			if( timeline[i] !== 1 ) timeline[i] = 2;
+		}
+
+		for( var i = 0 ; i < 23 ; i++ ) {
+			if( timeline[i] === 2 ) {
+				var from = i;
+				var to = i + 1;
+				for( var j = i + 1 ; j < 23 ; j++ ) {
+					if( timeline[j] !== 2 ) {
+						to = j;
+						i = j;
+						break;
+					}
+				}
+				resultTimeline.push({
+					from: from,
+					to: to
+				});
+			}
+		}
+
+		return resultTimeline;
 	}
 
 	Timeline.prototype.reducer = function(state, action) {
 		switch(action.type) {
-			// case 'CHOOSE_OFFICE_DATA_SUCCESS':
-			// 	return $.extend({}, state, {
-			// 		data: action.payload
-			// 	});
+			case 'UPDATE_NEXT_STATE':
+				return $.extend({}, state, {
+					nextState: action.payload
+				});
 			default:
 				return state;
 		}
@@ -129,12 +344,17 @@ $(function() {
 	var timelineDataArray = [
 		{
 			from: '9',
-			to: '10'
+			to: '12'
 		}
 	];
 
 	new Timeline('.timeline-1', {
 		data: timelineDataArray
+	});
+
+	$('.meeting-rooms-form-popup').switchPopup({
+		btnClass: 'js-tgl-meeting-popup',
+		duration: 300
 	});
 
 });
